a5a7eb419cc1ab67dbdf2e432b23cf44c5fe72a7
yleniapregnolato <yleniapregnolato@gmail.com>
2024-05-02 16:05:43 +0200

    add: components of the project

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <script>
-export default {
-  data() {
-    return {
+import AppFooter from './components/AppFooter.vue';
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
 
-    }
-  }
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter
+}
 }
 </script>
 
 <template>
   <h1>hello word</h1>
+  <AppHeader />
+  <AppMain />
+  <AppFooter />
 </template>
 
 <style lang="scss">
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <h2>Header</h2>
+</template>
+
+<style scoped lang="scss">
+</style>
