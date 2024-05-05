ce6e1563ce11cd384b43379caf1ba5f1496c3a47
yleniapregnolato <yleniapregnolato@gmail.com>
2024-05-05 23:20:05 +0200

    feat: footer icons

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -6,9 +6,16 @@ export default {
             shop: ["Shop DC", "Shop DC Collectibles"],
             dc: ["Terms Of Use", "Privacy policy (New)", "Ad Choices", "Advertising", "Jobs", "Subscriptions", "Talent Workshops", "CPSC Certificates", "Ratings", "Shop Help", "Contact Us"],
             sites: ["DC", "MAD Magazine", "DC Kids", "DC Universe", "DC Power Visa"],
+
+            footerIcons: ["footer-facebook.png", "footer-periscope.png", "footer-pinterest.png", "footer-twitter.png", "footer-youtube.png"],
         };
-    }
-}
+    },
+    methods: {
+        getIconUrl(iconsName) {
+            return new URL(`../assets/img/${iconsName}`, import.meta.url).href;
+        },
+    },
+};
 </script>
 
 <template>
@@ -48,7 +55,15 @@ export default {
             <!-- /first section -->
 
             <!-- second section -->
-            <section class="icons"></section>
+            <section class="icons">
+                <button type="button">SIGN-UP NOW!</button>
+                <div class="footer-icons">
+                    <h4>FOLLOW US</h4>
+                    <ul v-for="icon in footerIcons">
+                        <li><img :src="getIconUrl(icon)"></li>
+                    </ul>
+                </div>
+            </section>
             <!-- /second section -->
         </section>
 
@@ -90,14 +105,48 @@ footer {
                     text-decoration: none;
                     color: $text-footer;
                 }
+
+                &:hover {
+                    & a {
+                        color: white;
+                    }
+
+                }
             }
         }
 
 
     }
+
     .icons {
-        height: 10vh;
         background-color: $footer-background-color;
+        @include flex(row, space-between, center, nowrap);
+        padding-top: 10px;
+        padding-bottom: 10px;
+        h4 {
+            color: $primary-color;
+        }
+
+        button {
+            margin-left: 50px;
+            color: white;
+            background-color: $footer-background-color;
+            border: 1px solid $primary-color;
+            padding: 10px
+        }
+        .footer-icons {
+            @include flex(row, flex-end, center, nowrap);
+            gap: 20px;
+            margin-right: 50px;
+        }
+
+        ul {
+            li {
+                list-style-type: none;
+            }
+        }
+
+
     }
 }
 </style>
